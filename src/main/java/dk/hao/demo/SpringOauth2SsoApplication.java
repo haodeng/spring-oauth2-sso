dcc898f478c94094db7a6a404f358cfc0244c6cc
hao <hao.deng@autoremind.com>
2020-09-27 20:02:15 +0200

    first commit

---
+++ src/main/java/dk/hao/demo/SpringOauth2SsoApplication.java
@@ -0,0 +1,13 @@
+package dk.hao.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2SsoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2SsoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dk/hao/demo/SpringOauth2SsoApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.hao.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauth2SsoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
